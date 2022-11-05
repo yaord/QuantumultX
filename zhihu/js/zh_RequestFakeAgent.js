cec3141eb747ae46b1c270a07a57a6d1265db2b9
yaord <lxcl96@vip.qq.com>
2022-11-05 20:43:27 +0800

    知乎客户端6.0伪装成chrome

--- zhihu/js/zh_RequestFakeAgent.js
+++ zhihu/js/zh_RequestFakeAgent.js
@@ -1,11 +1,9 @@
-
-const headers = $request.headers;
-
 const url = $request.url;
+const headers = $request.headers;
+const answerIdRegx = /\d{7,10}/;
 const userAgent = 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36';
+
 try {
-    console.log("================================开始==========================");
-    
     delete headers['x-app-za'];
     delete headers['x-hybrid-config'];
     delete headers['X-Network-Type']
@@ -15,12 +13,11 @@ try {
     headers['dnt']=1;
     headers['upgrade-insecure-requests']=1;
     headers['exchange']='v=b3';
-    $notify("隐藏agent", "成功", "成功伪装成chrome!");
+    console.log("[yaord:SUCCESS ] answerId=" + url.match(answerIdRegx)[0]);
 } catch (error) {
-    console.log("[出现错误：]" + error);
+    console.log("[yaord:ERROR ] " + error);
+    $notify("客户端伪装失败", "error", "当前文章链接：" + url);
 }finally{
-    console.log("================================结束==========================");
     $done({ url:url,headers:headers});
-
 }
 

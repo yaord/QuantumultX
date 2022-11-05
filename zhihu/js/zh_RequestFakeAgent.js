3609f891e87beae1fb7965a8f108dc3c7cea0a95
yaord <lxcl96@vip.qq.com>
2022-11-05 16:41:09 +0800

    测试规则

--- zhihu/js/zh_RequestFakeAgent.js
+++ zhihu/js/zh_RequestFakeAgent.js
@@ -1,6 +1,10 @@
 
-const url = $request.url;
+const headers = $request.headers;
 const userAgent = 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36';
 
+
+console.log("请求头接受测试");
+console.log(headers);
+console.log(typeof headers);
 //完结时
 $done({ url });

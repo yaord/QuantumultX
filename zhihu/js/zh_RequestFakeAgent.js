a0a592ce1af9a772c9eb1a337b600d9678a78690
yaord <lxcl96@vip.qq.com>
2022-11-05 19:24:18 +0800

    测试规则

--- zhihu/js/zh_RequestFakeAgent.js
+++ zhihu/js/zh_RequestFakeAgent.js
@@ -5,9 +5,17 @@ const url = $request.url;
 const userAgent = 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36';
 
 
-console.log("请求头接受测试:headers为object类型！");
-
-console.log(JSON.parse(headers + ""));
-//完结时
+try {
+    console.log("================================开始==========================");
+    console.log('user-agent = ' + headers.user-agent);
+    console.log('accept-language = ' + headers.accept-language);
+    
+} catch (error) {
+    console.log("[出现错误：]" + error);
+}finally{
+    //完结时
 console.log("================================结束==========================");
 $done({ url:url,headers:headers});
+
+}
+

1e7023d5971016c60810df91a2ca7116ac28a00d
yaord <lxcl96@vip.qq.com>
2022-11-05 16:32:09 +0800

    测试规则

---
+++ README.md
@@ -0,0 +1,32 @@
+# 说明
+
++ 本仓库仅适用于QuantumultX规则
++ 本仓库为自用，不定期更新
+
+# 内容
+
++ 第三方懒人配置
+
+  > 见下：第三方仓库
+
++ 自用去广告脚本
+
+  > 见项目目录，以APP划分
+
+# 第三方仓库
+
++ **懒人配置：**[w37fhy/QuantumultX: 同步和更新大佬脚本库，更新懒人配置 (github.com)](https://github.com/w37fhy/QuantumultX)
++ **去广告**：[Orz-3/QuantumultX (github.com)](https://github.com/Orz-3/QuantumultX)
+
+# 官方仓库
+
+[crossutility/Quantumult-X (github.com)](https://github.com/crossutility/Quantumult-X)
+
+# Quantumult X 不完全教程
+
+[Quantumult X 不完全教程 (notion.so)](https://www.notion.so/Quantumult-X-1d32ddc6e61c4892ad2ec5ea47f00917)
+
+
+
+
+
---
+++ zhihu/js/zh_AnswerPageNoAD.js
@@ -0,0 +1,39 @@
+$notify("知乎","回答","新回答")
+console.warn("+++++++++++++++++++++++++++开始++++++++++++++++++++++++++++");
+
+const url = $request.url;
+let body = $response.body;
+const headRegx = /\<(\s)*div(\s)*class(\s)*\=(\s)*\"ListItemLoading\"(\s)*\>/;
+const allRegex = /\<(\s)*div(\s)*class(\s)*\=(\s)*\"ListItemLoading\"(\s)*\>(.|\n)+\<(\s)*div(\s)*class(\s)*\=(\s)*\"ListItemLoading\-img\"(\s)*\>(\s)*\<\/div\>(\s)*\<\/div\>(\s)*\<\/div\>/;
+body = filterAD();
+
+
+function filterAD() {
+    try {
+        let matchStr = body.match(headRegx);
+        if(matchStr == null | matchStr == undefined | matchStr == "") {
+            console.error("1.未找到最外层元素 class:ListItemLoading");
+            return body;
+        }
+
+        let target = body.match(allRegex)[0];
+        console.log("2.找到目标：" + target);
+        //只替换一次
+        return body.replace(target,"");
+    } catch (error) {
+        console.log("错误：" + error);
+    }
+    
+}
+
+console.error("+++++++++++++++++++++++++++结束++++++++++++++++++++++++++++");
+//完结时
+$done({ body });
+
+
+
+/*
+
+    参考文档：https://zhuanlan.zhihu.com/p/352724534
+ 
+*/

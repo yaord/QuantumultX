a162ffb3cff51a542a355b7f9de1dca4450f86e4
yaord <lxcl96@vip.qq.com>
2022-11-05 21:59:22 +0800

    知乎客户端6.0去除隐私访问请求

--- zhihu/js/zh_filterPrivacy.js
+++ zhihu/js/zh_filterPrivacy.js
@@ -4,7 +4,8 @@ const path3 = "/ad-style-service/request";
 const path4 = "/zst/events/r";
 const path5 = "/ab/api/v1/config";
 const path6 = "/me/guides";
-let url = $request.url;
+const url = $request.url;
+let headers = $request.headers;
 
 if(
     url.indexOf(path1) != -1|
@@ -14,7 +15,15 @@ if(
     url.indexOf(path5) != -1|
     url.indexOf(path6) != -1
 ) {
-    console.log("屏蔽隐私连接 :" + url);
-    url='';
+    
+    try {
+        for(let header in headers){
+            headers[header]='';
+        }
+        console.log("屏蔽隐私连接 :" + url);
+    } catch (error) {
+        console.log("屏蔽连接错误error= :" + error + " ,url=" + url);
+    }
+
 }
-$done({ url });
+$done({ url:url,headers:headers});

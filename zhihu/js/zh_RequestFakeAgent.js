1ca7ea250a9f825532e9be2c8d6bb0b8819fcb26
yaord <lxcl96@vip.qq.com>
2022-11-05 19:35:38 +0800

    测试规则

--- zhihu/js/zh_RequestFakeAgent.js
+++ zhihu/js/zh_RequestFakeAgent.js
@@ -3,12 +3,30 @@ const headers = $request.headers;
 
 const url = $request.url;
 const userAgent = 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36';
+/*
+    headers:
+        Connection:
+        Accept-Encoding:
+        x-app-za:
+        User-Agent:
+        x-api-version:
+        Authorization:
+        x-hybrid-config:
+        X-Network-Type:
+        Host:
+        x-app-version:
+        Cookie:
+        Accept-Language:
+        Accept:
+        X-MS-ID:
 
+*/
 
 try {
     console.log("================================开始==========================");
-    for(let obj in headers){
-        console.log(obj);
+    //打印headers
+    for(let header in headers){
+        console.log("【" + header + "=" + headers[header] + "】");
     }
 
 } catch (error) {

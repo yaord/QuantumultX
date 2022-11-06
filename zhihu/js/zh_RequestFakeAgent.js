00540880d5077b410269b290f5237947cddee759
yaord <lxcl96@vip.qq.com>
2022-11-06 11:08:51 +0800

    知乎客户端6.0去除问题回答列表中广告

--- zhihu/js/zh_RequestFakeAgent.js
+++ zhihu/js/zh_RequestFakeAgent.js
@@ -4,19 +4,23 @@ const answerIdRegx = /\d{7,10}/;
 const userAgent = 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36';
 
 try {
-    delete headers['x-app-za'];
-    delete headers['x-hybrid-config'];
-    delete headers['X-Network-Type']
-    delete headers['x-app-version'];
-    delete headers['X-MS-ID'];
-    delete headers['Authorization'];
 
-    //fakeHeaders();
+    if(headers['x-ad']|headers['x-ad-styles']) {
+        removeAD();
+    } else {
+        delete headers['x-app-za'];
+        delete headers['x-hybrid-config'];
+        delete headers['X-Network-Type']
+        delete headers['x-app-version'];
+        delete headers['X-MS-ID'];
+        delete headers['Authorization'];
 
-    headers['dnt']=1;
-    headers['upgrade-insecure-requests']=1;
-    headers['exchange']='v=b3';
-    console.log("[yaord:noAgent:SUCCESS ] answerId=" + url.match(answerIdRegx)[0]);
+        headers['dnt']=1;
+        headers['upgrade-insecure-requests']=1;
+        headers['exchange']='v=b3';
+        console.log("[yaord:noAgent:SUCCESS ] answerId=" + url.match(answerIdRegx)[0]);
+    }
+    
 } catch (error) {
     console.log("[yaord:noAgent:ERROR ] " + error);
     $notify("客户端伪装失败", "error", "当前文章链接：" + url);
@@ -24,34 +28,35 @@ try {
     $done({ headers:headers});
 }
 
-function fakeHeaders() {
+function removeAD() {
     //回答页
     // if(headers['X-APP-VersionCode']) {
     //     delete headers['X-APP-VersionCode'];
     // }
     // if(headers['X-Ab-Param']) {
-    //     delete headers['X-APP-VersionCode'];
+    //     delete headers['X-Ab-Param'];
     // }
     if(headers['x-ad']) {
         delete headers['x-ad'];
     }
-    if(headers['X-Zse-84']) {
-        delete headers['X-Zse-84'];
-    }
+    // if(headers['X-Zse-84']) {
+    //     delete headers['X-Zse-84'];
+    // }
     if(headers['X-B3-Traceid']) {
         delete headers['X-B3-Traceid'];
     }
-    // if(headers['x-ad-styles']) {
-    //     console.log("去除问题回答列表广告")
-    //     delete headers['x-ad-styles'];
-    // }
+    if(headers['x-ad-styles']) {
+        console.log("去除问题回答列表广告")
+        delete headers['x-ad-styles'];
+    }
     if(headers['X-UDID']) {
         delete headers['X-UDID'];
     }
-    if(headers['X-APP-Build']) {
-        delete headers['X-APP-Build'];
-    }
+    // if(headers['X-APP-Build']) {
+    //     delete headers['X-APP-Build'];
+    // }
     if(headers['X-SUGER']) {
         delete headers['X-SUGER'];
     }
+    console.log("[yaord:noQ&AListAD:SUCCESS ]");
 }

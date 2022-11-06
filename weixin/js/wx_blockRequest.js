c53cc266f9bb3987999263cbac447d41e315a992
yaord <lxcl96@vip.qq.com>
2022-11-06 19:27:22 +0800

    微信广告

--- weixin/js/wx_blockRequest.js
+++ weixin/js/wx_blockRequest.js
@@ -1,11 +1,16 @@
 const path1 = "/ads_svp_video_";
+const path2 = "/mmhead/";
 const url = $request.url;
 let headers = $request.headers;
 
 if(
-    url.indexOf(path1) != -1
+    url.indexOf(path1) != -1|
+    url.indexOf(path2) != -1
 ) {
-
+    console.log("+++++++++++++++++++++开始");
+    console.log("$request.path" + $request.path);
+    console.log("$request.scheme" + $request.scheme);
+    console.log("+++++++++++++++++++++结束");
     try {
         for(let header in headers){
             headers[header]='';

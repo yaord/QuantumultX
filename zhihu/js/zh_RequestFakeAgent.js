8dc276af720d4f17cbc39aa64c9a30fdbcc110ba
yaord <lxcl96@vip.qq.com>
2022-11-06 11:02:56 +0800

    知乎客户端6.0去除问题回答列表中广告

--- zhihu/js/zh_RequestFakeAgent.js
+++ zhihu/js/zh_RequestFakeAgent.js
@@ -11,36 +11,7 @@ try {
     delete headers['X-MS-ID'];
     delete headers['Authorization'];
 
-    //回答页
-    if(headers['X-APP-VersionCode']) {
-        console.log("去除问题回答列表广告")
-        delete headers['X-APP-VersionCode'];
-    }
-    if(headers['X-Ab-Param']) {
-        delete headers['X-APP-VersionCode'];
-    }
-    if(headers['x-ad']) {
-        delete headers['x-ad'];
-    }
-    if(headers['X-Zse-84']) {
-        delete headers['X-Zse-84'];
-    }
-    if(headers['X-B3-Traceid']) {
-        delete headers['X-B3-Traceid'];
-    }
-    if(headers['x-ad-styles']) {
-        console.log("去除问题回答列表广告")
-        delete headers['x-ad-styles'];
-    }
-    if(headers['X-UDID']) {
-        delete headers['X-UDID'];
-    }
-    if(headers['X-APP-Build']) {
-        delete headers['X-APP-Build'];
-    }
-    if(headers['X-SUGER']) {
-        delete headers['X-SUGER'];
-    }
+    //fakeHeaders();
 
     headers['dnt']=1;
     headers['upgrade-insecure-requests']=1;
@@ -53,3 +24,34 @@ try {
     $done({ headers:headers});
 }
 
+function fakeHeaders() {
+    //回答页
+    // if(headers['X-APP-VersionCode']) {
+    //     delete headers['X-APP-VersionCode'];
+    // }
+    // if(headers['X-Ab-Param']) {
+    //     delete headers['X-APP-VersionCode'];
+    // }
+    if(headers['x-ad']) {
+        delete headers['x-ad'];
+    }
+    if(headers['X-Zse-84']) {
+        delete headers['X-Zse-84'];
+    }
+    if(headers['X-B3-Traceid']) {
+        delete headers['X-B3-Traceid'];
+    }
+    // if(headers['x-ad-styles']) {
+    //     console.log("去除问题回答列表广告")
+    //     delete headers['x-ad-styles'];
+    // }
+    if(headers['X-UDID']) {
+        delete headers['X-UDID'];
+    }
+    if(headers['X-APP-Build']) {
+        delete headers['X-APP-Build'];
+    }
+    if(headers['X-SUGER']) {
+        delete headers['X-SUGER'];
+    }
+}

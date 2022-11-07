ec937cac9099ead93e78c424fb24dea6cf7e9375
yaord <117494674+yaord@users.noreply.github.com>
2022-11-07 21:38:07 +0800

    Update zh_RequestFakeAgent.js

--- zhihu/js/zh_RequestFakeAgent.js
+++ zhihu/js/zh_RequestFakeAgent.js
@@ -1,30 +1,30 @@
 const url = $request.url;
 let headers = $request.headers;
-const answerIdRegx = /\d{7,10}/;
+//const answerIdRegx = /\d{7,10}/;
 const userAgent = 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36';
 
 try {
 
-    if(headers['X-Ab-Param']!=null|headers['x-ad'] != null|headers['x-ad-styles'] != null) {
+    //if(headers['X-Ab-Param']!=null|headers['x-ad'] != null|headers['x-ad-styles'] != null) {
         //console.log("request去除问题回答列表广告")
-        removeAD();
-    } else {
-        delete headers['x-app-za'];
-        delete headers['x-hybrid-config'];
-        delete headers['X-Network-Type']
-        delete headers['x-app-version'];
-        delete headers['X-MS-ID'];
-        delete headers['Authorization'];
-
-        headers['dnt']=1;
-        headers['upgrade-insecure-requests']=1;
-        headers['exchange']='v=b3';
-        console.log("[zh:noAgent:SUCCESS ] answerId=" + url.match(answerIdRegx)[0]);
-    }
+        //removeAD();
+    //} else {
+        //delete headers['x-app-za'];
+        //delete headers['x-hybrid-config'];
+        //delete headers['X-Network-Type']
+        //delete headers['x-app-version'];
+        //delete headers['X-MS-ID'];
+        //delete headers['Authorization'];
+        headers['User-Agent']=userAgent;
+        //headers['dnt']=1;
+        //headers['upgrade-insecure-requests']=1;
+        //headers['exchange']='v=b3';
+        //console.log("[zh:noAgent:SUCCESS ] answerId=" + url.match(answerIdRegx)[0]);
+   // }
     
 } catch (error) {
     console.log("[zh:noAgent:ERROR ] " + error);
-    $notify("客户端伪装失败", "error", "当前文章链接：" + url);
+    //$notify("客户端伪装失败", "error", "当前文章链接：" + url);
 }finally{
     $done({ headers:headers});
 }
